251c8293f51e427d1ee0d847b55e4ee21835b866
abdulrahmon <rahmonabdul297@gmail.com>
2026-05-25 09:08:43 +0100

    clean up

--- src/app/Arrays.js
+++ src/app/Arrays.js
@@ -12,7 +12,6 @@ export const AdminPortalNavs = [
   { id: 2, icon: "📋", url: "/admin/results", navs: "term results" },
   { id: 3, icon: "📈", url: "/admin/overall-results", navs: "overall results" },
   { id: 4, icon: "🔍", url: "/checkResult", navs: "result check" },
-  { id: 5, icon: "📷", url: "/admin/passports", navs: "passports" },
-  { id: 6, icon: "👨‍🏫", url: "/admin/teachers", navs: "class teachers" },
-  { id: 7, icon: "⚙️", url: "/admin/settings", navs: "settings" },
+  { id: 5, icon: "👨‍🏫", url: "/admin/teachers", navs: "class teachers" },
+  { id: 6, icon: "⚙️", url: "/admin/settings", navs: "settings" },
 ];
